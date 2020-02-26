f856b3fccab373c3b8abccf8918c5af8be09ae50
jimmy shah <jimmyshah83@gmail.com>
2020-02-26 17:13:48 -0500

    Initial commit

---
+++ src/test/java/com/pivotal/jishah/boot/practice/bootpractice/BootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pivotal.jishah.boot.practice.bootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
